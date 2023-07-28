23196ae56d4fd07c1538c6520acb69b7975608e1
ramaciotti69 <joao.ramaciotti@aluno.senai.br>
2023-07-28 10:49:05 -0300

    Criando variaveis em Java

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -14,7 +14,11 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     String nome = "Moisés";
+     int idade = 40;
+     boolean estudante = true;
+     double media = 5.5;
+     
     }
     
 }
