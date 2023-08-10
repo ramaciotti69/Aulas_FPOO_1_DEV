e9cbdbe3e3c8ff9e451ed27beaa06c34ac4b41e4
SchiabelC <caio.schiaebelc@gmail.com>
2023-08-10 11:27:47 -0300

    Atualizado

--- Fundamentos/src/estruturadecisao/Atividade1.java
+++ Fundamentos/src/estruturadecisao/Atividade1.java
@@ -3,51 +3,51 @@ package estruturadecisao;
 import java.util.Scanner;
 
 public class Atividade1 {
-public static void main(String[] args) {
-    try (Scanner scanner = new Scanner(System.in)) {
-        System.out.print("Digite o seu cargo do funcionário (Gerente/Supervisor/Tecnico/Auxiliar): ");
-        String cargo = scanner.nextLine();
 
-        double salarioBase = 0;
-        double inssPorcentagem = 0;
-        double convenio = 0;
+    public static void main(String[] args) {
+        try ( Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite o seu cargo do funcionário (Gerente/Supervisor/Tecnico/Auxiliar): ");
+            String cargo = scanner.nextLine();
 
-        switch (cargo.toLowerCase()) {
-            case "gerente":
-                salarioBase = 5590.00;
-                inssPorcentagem = 8.0;
-                convenio = 289.00;
-                break;
-            case "supervisor":
-                salarioBase = 4128.00;
-                inssPorcentagem = 7.0;
-                convenio = 239.00;
-                break;
-            case "tecnico":
-                salarioBase = 3789.00;
-                inssPorcentagem = 4.0;
-                convenio = 189.00;
-                break;
-            case "auxiliar":
-                salarioBase = 2345.00;
-                inssPorcentagem = 2.0;
-                convenio = 156.00;
-                break;
-            default:
-                System.out.println("Cargo invalido!");
-                scanner.close();
-                return;
+            double salarioBase = 0;
+            double inssPorcentagem = 0;
+            double convenio = 0;
+
+            switch (cargo.toLowerCase()) {
+                case "gerente":
+                    salarioBase = 5590.00;
+                    inssPorcentagem = 8.0;
+                    convenio = 289.00;
+                    break;
+                case "supervisor":
+                    salarioBase = 4128.00;
+                    inssPorcentagem = 7.0;
+                    convenio = 239.00;
+                    break;
+                case "tecnico":
+                    salarioBase = 3789.00;
+                    inssPorcentagem = 4.0;
+                    convenio = 189.00;
+                    break;
+                case "auxiliar":
+                    salarioBase = 2345.00;
+                    inssPorcentagem = 2.0;
+                    convenio = 156.00;
+                    break;
+                default:
+                    System.out.println("Cargo nao identificado");
+                    scanner.close();
+                    return;
+            }
+
+            double descontoINSS = salarioBase * (inssPorcentagem / 100);
+            double salarioLiquido = salarioBase - descontoINSS - convenio;
+
+            System.out.println("Cargo: " + cargo);
+            System.out.println("Salario Base: R$" + salarioBase);
+            System.out.println("Desconto INSS: R$" + descontoINSS);
+            System.out.println("Convenio: R$" + convenio);
+            System.out.println("Salario Liquido: R$" + salarioLiquido);
         }
-
-        double descontoINSS = salarioBase * (inssPorcentagem / 100);
-        double salarioLiquido = salarioBase - descontoINSS - convenio;
-
-        System.out.println("Cargo: " + cargo);
-        System.out.println("Salario Base: R$ " + salarioBase);
-        System.out.println("Desconto INSS: R$ " + descontoINSS);
-        System.out.println("Convenio: R$ " + convenio);
-        System.out.println("Salario Liquido: R$ " + salarioLiquido);
-    }
     }
 }
-
--- Fundamentos/src/estruturadecisao/Atividade2.java
+++ Fundamentos/src/estruturadecisao/Atividade2.java
@@ -1,5 +1,33 @@
 package estruturadecisao;
 
+import java.util.Scanner;
+
 public class Atividade2 {
+
+    public static void main(String[] args) {
+        try ( Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite a largura do terreno em metros: ");
+            double largura = scanner.nextDouble();
+            System.out.print("Digite o comprimento do terreno em metros: ");
+            double comprimento = scanner.nextDouble();
+
+            double area = largura * comprimento;
+            double valorMetroQuadrado;
+
+            if (area <= 250) {
+                valorMetroQuadrado = 8.5;
+             } else if (area <= 350) {
+                valorMetroQuadrado = 9.5;
+               } else {
+                valorMetroQuadrado = 11.5;
+            }
+
+            double valorTotal = area * valorMetroQuadrado;
+
+               System.out.println("Area do terreno: " + area + " m²");
+              System.out.println("Valor por metro quadrado: R$" + valorMetroQuadrado);
+            System.out.println("Valor total a ser pago: R$" + valorTotal);
+        }
+    }
 
 }
