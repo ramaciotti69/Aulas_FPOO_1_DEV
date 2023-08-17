62c93e085ca6fad146cc5a724ca7b345a9784f02
ramaciotti69 <joao.ramaciotti@aluno.senai.br>
2023-08-17 09:12:48 -0300

    Atualizado

--- Fundamentos/src/enumeracao/Usuario.java
+++ Fundamentos/src/enumeracao/Usuario.java
@@ -9,5 +9,25 @@ package enumeracao;
  * @author Aluno
  */
 public class Usuario {
+
+   
+    private String nome;
+    private String email;
+    private boolean ativo;
+    private TipoUsuarioEnum tipo;
+
+   
+    
+    public Usuario(String nome, String email, boolean ativo, TipoUsuarioEnum tipo) {
+        this.nome = nome;
+        this.email = email;
+        this.ativo = ativo;
+        this.tipo = tipo;
+    }
+    
+     @Override
+    public String toString() {
+        return "Usuario{" + "nome=" + nome + ", email=" + email + ", ativo=" + ativo + ", tipo=" + tipo + '}';
+    }
     
 }
