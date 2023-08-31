621bc72eda9d4db68608d398e1c7e325b152e424
ramaciotti69 <joao.ramaciotti@aluno.senai.br>
2023-08-31 09:24:49 -0300

    Atualização

--- Fundamentos/src/orientacaoobjetos/Locadora.java
+++ Fundamentos/src/orientacaoobjetos/Locadora.java
@@ -20,12 +20,17 @@ public class Locadora {
         System.out.println(c1);
         double valorLocacaoSimples = c1.calcularLocacao(3);
         System.out.println("O valor para a locação do filme é: " + valorLocacaoSimples);
+        c1.validarNome(c1.getNome());
+         c1.verificarEmailDominioGoogle(c1.getEmail());
 
         Cliente c2 = new Cliente("Ronalda Márta", "ronaldamarta@gmail.com");
         System.out.println(c2);
         double valorLocacaoLancamento = c2.calcularLocacao(3, true);
         System.out.println("O valor para a locação do filme de lançamento é: " + valorLocacaoLancamento);
-
+ c2.validarNome(c2.getNome());
+         c2.verificarEmailDominioGoogle(c2.getEmail());
     }
-   
+    
+    
+    
 }
